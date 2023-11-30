949875bdd96461d611da27959e529efa97ca245d
Bruno Boi <bruno.boi@bobtopus.eu>
2023-11-30 14:16:07 +0100

    feat: create a devenv for odoo development

---
+++ README.md
@@ -0,0 +1,107 @@
+# Odoo Development Environment
+
+This repository contains a development environment for Odoo.
+It is based on nix flakes and uses [devenv](https://devenv.sh)
+as a development environment manager.
+
+## Getting started
+
+### Install nix
+
+```bash
+sh <(curl -L https://nixos.org/nix/install) --daemon
+```
+
+### Install [cachix](https://cachix.org/) (optional)
+
+Recommended, speeds up the installation by providing binaries.
+
+```bash
+nix profile install nixpkgs#cachix
+cachix use devenv
+```
+
+### Install [devenv](https://devenv.sh)
+
+```bash
+nix profile install --accept-flake-config tarball+https://install.devenv.sh/latest
+```
+
+### Clone this repository
+
+This will clone the repository and all submodules.
+This will take a while, as the submodules are quite large.
+In order to speed things up, we use:
+
+- the `--jobs` option to clone submodules in parallel
+- the `--shallow-submodules` option to only clone with a depth of 1
+
+The `--remote-submodules` option is used to clone the submodules from the
+remote repository instead of the local one, which will ensure that the
+submodules are up to date with the latest commits from the remote repositories.
+This will probably result in a dirty git status, as the submodules SHA1 will
+be different from the references in this repository's submodules git history.
+
+```bash
+git clone --recurse-submodules --remote-submodules --shallow-submodules --jobs=8 git@github.com:brboi/odoo-dev.git
+```
+
+In case of failure to fetch submodules, you can run later at
+the root of the repository directory:
+
+```bash
+git submodule update --init --recursive --remote
+```
+
+You won't benefit of the parallel cloning though.
+
+### Activate the development environment
+
+It will automatically install all dependencies.
+
+```bash
+cd odoo-dev
+devenv shell
+```
+
+You may also want an automatic shell activation.
+For this you will need another tool called [direnv](https://direnv.net/).
+See this [devenv documentation](https://devenv.sh/automatic-shell-activation/).
+Once set up, you can allow the automatic shell activation with:
+
+```bash
+direnv allow
+```
+
+#### In case of failure
+
+If the `./odoo/community/requirements.txt` installation fails, you can try
+to install `./requirements.txt` file afterward.
+
+```bash
+pip install -r requirements.txt
+```
+
+Instead you could also temporarily update the content of
+`./odoo/community/requirements.txt` and then run the following command
+to retry the installation of all the required dependencies.
+
+```bash
+pip install -r odoo/community/requirements.txt
+```
+
+## Usage
+
+### Start the development environment processes
+
+This will start postgresql.
+
+```bash
+devenv up
+```
+
+### CLI tool (work in progress)
+
+```bash
+odoo-dev start odoo -i web
+```
---
+++ flake.nix
@@ -0,0 +1,200 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    # https://lazamar.co.uk/nix-versions/?package=wkhtmltopdf&version=0.12.5&fullName=wkhtmltopdf-0.12.5&keyName=wkhtmltopdf&revision=096bad8d75f1445e622d07faccce2e8ff43956c5&channel=nixos-20.03#instructions
+    nixos-20_03-pkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/096bad8d75f1445e622d07faccce2e8ff43956c5.tar.gz";
+    }) {};
+  in {
+    devenv-up = self.devShell.x86_64-linux.config.procfileScript;
+    devShell."x86_64-linux" = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        {
+          pre-commit.hooks = {
+            alejandra.enable = true;
+            commitizen.enable = true;
+            mdsh.enable = true;
+            prettier.enable = true;
+          };
+
+          packages = with pkgs; [
+            bashInteractive
+            git
+            nodejs_20
+
+            # wkhtmltopdf 0.12.5
+            nixos-20_03-pkgs.wkhtmltopdf
+
+            # Odoo deps for requirements.txt
+            cyrus_sasl.dev
+            gcc
+            gsasl
+            openldap
+
+            # Python
+            python311Packages.ipdb
+            python311Packages.libsass
+            python311Packages.pip
+            python311Packages.pq
+            python311Packages.pyopenssl
+            python311Packages.pypdf2
+            python311Packages.python-ldap
+            python311Packages.setuptools
+            python311Packages.virtualenv
+          ];
+
+          languages = {
+            nix.enable = true;
+            javascript.enable = true;
+            python = {
+              enable = true;
+              version = "3.11";
+              venv.enable = true;
+            };
+          };
+
+          enterShell = ''
+            export PATH=result/local/bin:$PATH
+            export PYTHONPATH=`pwd`/$VENV/${pkgs.python.sitePackages}/:$PYTHONPATH
+
+            # Allow the use of wheels.
+            SOURCE_DATE_EPOCH=$(date +%s)
+            VENV=.venv
+            REQUIREMENTS=odoo/community/requirements.txt
+            if test ! -d $VENV; then
+              # First time, create a virtualenv to use for building.
+              python -m venv $VENV
+              source $VENV/bin/activate
+
+              # Let's install some useful packages.
+              pip install wheel
+              pip install inotify ipython
+              npm install rtlcss
+
+              if test ! -f $REQUIREMENTS; then
+                # If there is no requirements.txt,
+                # we need to initialize the submodules first.
+                git submodule update --init --recursive --remote
+              fi
+
+              # Install odoo requirements.
+              pip install -r $REQUIREMENTS
+
+              # Setup proper submodules remotes
+              pushd odoo/community
+              git config remote.origin.url git@github.com:odoo/odoo.git
+              git config remote.origin.pushurl git@github.com:odoo/odoo.git
+              git config remote.dev.url git@github.com:odoo-dev/odoo.git
+              git config remote.dev.pushurl git@github.com:odoo-dev/odoo.git
+              git config --add remote.dev.fetch '+refs/heads/*:refs/remotes/dev/*'
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/enterprise
+              git config remote.origin.url git@github.com:odoo/enterprise.git
+              git config remote.origin.pushurl git@github.com:odoo/enterprise.git
+              git config remote.dev.url git@github.com:odoo-dev/enterprise.git
+              git config remote.dev.pushurl git@github.com:odoo-dev/enterprise.git
+              git config --add remote.dev.fetch '+refs/heads/*:refs/remotes/dev/*'
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/industry
+              git config remote.origin.url git@github.com:odoo/industry.git
+              git config remote.origin.pushurl git@github.com:odoo/industry.git
+              git config remote.dev.url git@github.com:odoo-dev/industry.git
+              git config remote.dev.pushurl git@github.com:odoo-dev/industry.git
+              git config --add remote.dev.fetch '+refs/heads/*:refs/remotes/dev/*'
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/design-themes
+              git config remote.origin.url git@github.com:odoo/design-themes.git
+              git config remote.origin.pushurl git@github.com:odoo/design-themes.git
+              git config remote.dev.url git@github.com:odoo-dev/design-themes.git
+              git config remote.dev.pushurl git@github.com:odoo-dev/design-themes.git
+              git config --add remote.dev.fetch '+refs/heads/*:refs/remotes/dev/*'
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/upgrade-util
+              git config remote.origin.url git@github.com:odoo/upgrade-util.git
+              git config remote.origin.pushurl git@github.com:odoo/upgrade-util.git
+              git config remote.dev.url git@github.com:odoo-dev/upgrade-util.git
+              git config remote.dev.pushurl git@github.com:odoo-dev/upgrade-util.git
+              git config --add remote.dev.fetch '+refs/heads/*:refs/remotes/dev/*'
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/upgrade
+              git config remote.origin.url git@github.com:odoo/upgrade.git
+              git config remote.origin.pushurl git@github.com:odoo/upgrade.git
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+
+              pushd odoo/documentation
+              git config remote.origin.url git@github.com:odoo/documentation.git
+              git config remote.origin.pushurl git@github.com:odoo/documentation.git
+              git remote set-branches origin '*'
+              git fetch origin master
+              git checkout master
+              popd
+            fi
+            source $VENV/bin/activate
+          '';
+
+          services.postgres = {
+            enable = true;
+            package = pkgs.postgresql_14;
+            initialScript = ''
+              CREATE USER postgres WITH
+                SUPERUSER
+                PASSWORD 'PostgresqlSuperAdminP@ssw0rd';
+              CREATE USER odoo     WITH
+                CREATEDB
+                PASSWORD 'odoo';
+            '';
+          };
+
+          scripts = {
+            # TODO: make this script user friendly with nice CLI options.
+            odoo-dev.exec = ''
+              ./odoo/community/odoo-bin -c ./odoo.conf --dev=all -d odoo -i web
+            '';
+          };
+        }
+      ];
+    };
+  };
+}
