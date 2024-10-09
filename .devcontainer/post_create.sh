24f23fbdc97e3cf28e44bd537233395dd2d8835a
Bruno Boi (boi) <boi@odoo.com>
2024-10-09 08:08:16 +0000

    feat: move scripts in container and call at create
    Some scripts were only available on the workspace's scripts folder.
    They are now moved into the devcontainer.
    
    Furthermore the post_create.sh script was called every the container
    was started, instead of at creation only.
    
    The post_create.sh script now also takes care of setting up the odoo
    community repository + pre-commit hooks of the dev repo.

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Setting access rights
 sudo chown -R odoo:odoo /etc/odoo /var/run/odoo
 mkdir -p /commandhistory
 sudo chown -R odoo:odoo /commandhistory
@@ -8,10 +9,15 @@ touch /commandhistory/.bash_history
 touch /commandhistory/.zsh_history
 sudo chown -R odoo:odoo /commandhistory
 
+# Bootstrapping command history
 SNIPPET="export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history"
 SNIPPET_ZSH="autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/commandhistory/.zsh_history"
-
 echo "$SNIPPET" >> /home/odoo/.bashrc
 echo "$SNIPPET_ZSH" >> /home/odoo/.zshrc
 
-install-odoo-requirements
+# Setting up Odoo community repo
+.devcontainer/copy/bin/setup-odoo-repository odoo community
+.devcontainer/copy/bin/install-odoo-requirements
+
+# Setting up pre-commit hooks
+.devcontainer/copy/bin/setup-pre-commit
